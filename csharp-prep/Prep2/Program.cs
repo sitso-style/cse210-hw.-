cb69f9de0d4690b058d0acca25979ff591707952
sitso-style <ash23009@byui.edu>
2023-11-04 23:38:51 -0600

    Finised Prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,40 @@
 using System;
 
-class Program
+Console.Write("What is your grade percentage? ");
+string answer = Console.ReadLine();
+int percent = int.Parse(answer);
+
+string letter = "";
+
+if (percent >= 90)
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep2 World!");
-    }
+    letter = "A";
+}
+else if (percent >= 80)
+{
+    letter = "B";
+}
+else if (percent >= 70)
+{
+    letter = "c";
+}
+else if (percent >= 60)
+{
+    letter = "D";
+}
+else
+{
+    letter = "F";
+}
+
+Console.WriteLine($"your grade is: {letter}");
+
+if (percent >= 70)
+{
+    Console.WriteLine("Congratulations! You passed the course.");
+
+}
+else
+{
+    Console.WriteLine("Sorry, you did not pass this course.");
 }
